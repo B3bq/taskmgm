d2cffbc031b1551e6d1074a18bee81d66186fd10
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-08-17 22:48:21 +0200

    main page view start

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// toggle menu script
+const toggleMenuEl = document.getElementById('js-toggle-menu');
+const toggleableMenuEl = document.getElementById('js-toggleable-menu');
+
+toggleMenuEl?.addEventListener('click', function(){
+    toggleableMenuEl?.classList.toggle('active');
+})
