57aa6b04a3f384e8ad0f9c57ff3139e693f4efd2
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-08-18 23:59:56 +0200

    checkboxes and hover on ?

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,15 @@ const toggleableMenuEl = document.getElementById('js-toggleable-menu');
 toggleMenuEl?.addEventListener('click', function(){
     toggleableMenuEl?.classList.toggle('active');
 })
+
+const checkBoxes = document.querySelectorAll('input[type="checkbox"][name="option"');
+
+checkBoxes.forEach(cb =>{
+    cb.addEventListener('change', () => {
+        if(cb.checked){
+            checkBoxes.forEach(other => {
+                if(other !== cb) other.checked = false;
+            })
+        }
+    })
+})
