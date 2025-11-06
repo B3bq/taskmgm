69a5f41cd87b6d3f015d4ac4452757959378bb33
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-11-06 23:15:30 +0100

    not mail :(

--- app.py
+++ app.py
@@ -679,8 +679,8 @@ def insert():
                 hashed_str = hashed.decode('utf-8')
 
                 code = random.randrange(1000, 9999)
-                sent = mail_sent(mail=mail, code=code)
-                #sent = True
+                #sent = mail_sent(mail=mail, code=code)
+                sent = True
 
                 # seve in session            
                 session['username'] = user
@@ -690,8 +690,8 @@ def insert():
                 session['from'] = "sign"
 
                 if sent:
-                    return render_template('code.html')
-                    #return redirect(url_for('index'))
+                    #return render_template('code.html')
+                    return redirect(url_for('index'))
                 else:
                     text = "Something wrong"
                     return render_template('sign.html', text=text)
